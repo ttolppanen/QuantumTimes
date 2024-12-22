a6d64209a30396d13fd2bb5265c6353ef613a694
ttolppanen <taneli.tolppanen@gmail.com>
2024-12-23 00:15:08 +0200

    added everything

--- src/QuantumTimes.jl
+++ src/QuantumTimes.jl
@@ -1,5 +1,43 @@
 module QuantumTimes
 
-greet() = print("Hello World!")
+using GLM
+
+export exp_fit_model
+export exp_fit
+export t_time
+export t2
+
+function exp_fit_model(x, y, c = nothing)
+    y_log = log.(y)
+    if isa(c, Nothing)
+        model = lm(@formula(y_log ~ x), (; x, y_log))
+    else
+        y_log = y_log .- log(c)
+        model = lm(@formula(y_log ~ 0 + x), (; x, y_log))
+    end
+    return model
+end
+
+function exp_fit(x, y, c = nothing)
+    model = exp_fit_model(x, y, c)
+    if isa(c, Nothing)
+        intercept = exp(coef(model)[1])
+        slope = coef(model)[2]
+    else
+        intercept = c
+        slope = coef(model)[1]
+    end
+    return intercept, slope, intercept * exp.(slope .* x)
+end
+
+function t_time(x, y, c = nothing)
+    model = exp_fit_model(x, y, c)
+    slope = coef(model)[2]
+    return -1 / slope
+end
+
+function t2(t1, pure_t2)
+    return 1 / ((1 / (2*t1)) + (1/pure_t2))
+end
 
 end # module QuantumTimes
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,41 @@
-using Test
+using Test, QuantumTimes
+using Plots
+
+@testset "QuantumTimes" begin
+    
+    @testset "exp_fit" begin
+        intercept = rand()
+        slope = 1 + rand()
+        N = 50
+        x = range(0, 100, length = N)
+        y = intercept * exp.(slope * (x .+ rand(N)))
+        r_int, r_slp, fit = exp_fit(x, y)
+        pl = plot(x, y, st = :scatter, yaxis = :log10)
+        plot!(pl, x, fit)
+        savefig(joinpath(@__DIR__, "exp_fit"))
+
+        r_int, r_slp, fit = exp_fit(x, y, intercept)
+        pl = plot(x, y, st = :scatter, yaxis = :log10)
+        plot!(pl, x, fit)
+        savefig(joinpath(@__DIR__, "exp_fit_c"))
+        @test fit[1] == intercept
+        @test intercept == r_int
+    end
+
+    @testset "t_time" begin
+        intercept = rand()
+        slope = -1 / (1 + rand())
+        N = 50
+        x = range(0, 100, length = N)
+        y = intercept * exp.(slope * (x .+ rand(N)))
+        t = t_time(x, y)
+        @test round(-1 / slope, digits=1) == round(t, digits=1)  
+    end
+
+    @testset "t2" begin
+        t1 = 1 + rand()
+        pure_t2 = 1 + rand()
+        @test 1 / t2(t1, pure_t2) == (1 / (2*t1) + 1 / (pure_t2))
+    end
+
+end # testset 
